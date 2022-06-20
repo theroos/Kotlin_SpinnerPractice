937a1193bb048cad6a6ef62ab724738ae081352b
theroos <roosonline7@gmail.com>
2022-06-21 01:01:59 +0530

    Second Commit

--- app/src/main/java/com/example/theroos/spinner_test/MainActivity.kt
+++ app/src/main/java/com/example/theroos/spinner_test/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import com.example.theroos.myapplication.R
+import com.example.theroos.spinner_test.R
 
 class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
 
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
 
         val months = resources.getStringArray(R.array.Months)
 
-        txt_msg = findViewById<TextView>(R.id.spinner_msg)
         spinner = findViewById<Spinner>(R.id.spinner_test)
 
         if (spinner != null) {
@@ -28,6 +27,9 @@ class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
             spinner!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, p3: Long) {
                     Toast.makeText(this@MainActivity,getString(R.string.selected_item) + " " + "" + months[position], Toast.LENGTH_SHORT).show()
+                    var txt_msg: TextView = findViewById<TextView>(R.id.spinner_msg)
+                    var mnth: String = months[position].toString()
+                    txt_msg.setText(mnth)
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
